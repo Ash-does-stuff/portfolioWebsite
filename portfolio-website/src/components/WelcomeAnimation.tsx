9ca89b5911eac062d6afeec36db128bd1571fd51
Ashley <ashhh.does.stuff@gmail.com>
2023-11-25 22:43:33 +0100

    changess
    yaah

--- portfolio-website/src/components/WelcomeAnimation.tsx
+++ portfolio-website/src/components/WelcomeAnimation.tsx
@@ -3,7 +3,7 @@ import "../styles/main.css"
 function WelcomeAnimation() {
   return (
     <div className="anim-bg">
-      <img src={require("../assets/sphere.gif")} className="anim-img"/>
+      <img src={require("../assets/sphere.gif")} className="anim-img" alt="Welcome (heyaa :3)"/>
     </div>
   );
 }
--- portfolio-website/src/pages/PageHome.tsx
+++ portfolio-website/src/pages/PageHome.tsx
@@ -1,9 +1,16 @@
-import "../styles/main.css"
+import { Widget } from "discord.js";
+import "../styles/main.css";
 
 function PageHome() {
   return (
     <div className="container">
-      <h1>dfsgdf</h1>
+      <div className="row centerAlign">
+        <div className="column" style={{marginRight: "10vw"}}>
+            <h1 className="resetMargin">Hey there!</h1>
+            <h4 className="resetMargin">welcum to my page :333</h4>
+        </div>
+        <img src={require("../assets/wave.png")} className="zoomOnHover" style={{width: "35%", height: "35%"}}/>
+      </div>
     </div>
   );
 }
