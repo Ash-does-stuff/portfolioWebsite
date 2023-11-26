6be341d0e476ea15cefd12279eac7114fb38ddb4
Ashley <ashhh.does.stuff@gmail.com>
2023-11-26 21:38:12 +0100

    uh

--- portfolio-website/src/components/MusicStats.tsx
+++ portfolio-website/src/components/MusicStats.tsx
@@ -3,6 +3,7 @@ const Spotify = require('spotify-web-api-js');
 function MusicStats() {
 
   //to-do lol have to figure out token refreshing, prolly gonna steal the spotifyClient requesting code
+  //https://www.npmjs.com/package/spotify-web-api-js#player
 
   var spotifyApi = new Spotify();
   spotifyApi.setAccessToken('BQBRBg4ProlC3K9YUpKFXMlqaXbxphZXg9KoMxncLBiAKHZV7CN6lbYxvXJNZYXqpausCcbA0rJatgG6CSSBXvIKn7QnNMYA4stNXkv9Xa24V-nV2xnNQOGKH-AhqUv6dopbnx9N50PAPXb7Vnsh4nH6Y_3RCEZuqSjpsrMV2BKZHWz4kITKFhAp176oBIG1GEMBb7Oup-0bn-e7HfbIDAc71Vk');
