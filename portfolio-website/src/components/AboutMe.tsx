a5ad010ba43a4a70e8da9e2ea78b2ba1e52bde2b
Ashley <151153610+Ash-does-stuff@users.noreply.github.com>
2023-11-27 16:35:30 +0100

    I may have figured out flex positioning
    just maybe

--- portfolio-website/src/components/AboutMe.tsx
+++ portfolio-website/src/components/AboutMe.tsx
@@ -1,22 +1,20 @@
 function AboutMe() {
   return (
-    <div
-      className="row centerAlign"
-      style={{ width: "100vw", height: "100vh" }}
-      id="header"
-    >
-      <div className="column">
-        <div className="row centerAlign">
-          <img src={require("../assets/uwuHiBig.png")} />
-          <div className="column">
-          <h1 className="resetMargin" style={{ fontSize: "4vw" }}>
-            HAAAAAAIIIIIII
-          </h1>
-          <h4 className="resetMargin indentation">welcum to my page :333</h4>
+      <div className="column centerAlign aboutMe">
+        <div className="row" style={{}}>
+          <div style={{flex: 3, display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <img src={require("../assets/uwuHiBig.png")} />
+          </div>
+          <div style={{flex:2, display: "flex", alignItems: "center", justifyContent: "flex-start"}}>
+            <div className="column">
+              <h1 className="resetMargin" style={{ fontSize: "3vw" }}>
+                About me
+              </h1>
+              <h4 className="resetMargin indentation">I kinda code <br /> I guess</h4>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div >
   );
 }
 
