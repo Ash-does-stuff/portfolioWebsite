f15d898480f76543a035dc2c1d181fd9dd188c45
Ashley <151153610+Ash-does-stuff@users.noreply.github.com>
2023-11-28 10:34:24 +0100

    changes

--- portfolio-website/src/components/AboutMe.tsx
+++ portfolio-website/src/components/AboutMe.tsx
@@ -1,7 +1,8 @@
 function AboutMe() {
   return (
       <div className="column centerAlign aboutMe">
-        <div className="row" style={{width: "100%"}}>
+
+        <div className="row" style={{width: "100%", margin: "1vw"}}>
           <div style={{flex: 3, display: "flex", alignItems: "center", justifyContent: "center"}}>
             <img src={require("../assets/uwuHiBig.png")} />
           </div>
@@ -14,6 +15,24 @@ function AboutMe() {
             </div>
           </div>
         </div>
+
+        <div className="row" style={{width: "100%", margin: "1vw"}}>
+          
+          <div style={{flex:2, display: "flex", alignItems: "center", justifyContent: "flex-end"}}>
+            <div className="column">
+              <h1 className="resetMargin" style={{ fontSize: "3vw" }}>
+                About me 2
+              </h1>
+              <h4 className="resetMargin indentation">I also kinda like music<br /> I guess</h4>
+            </div>
+          </div>
+
+          <div style={{flex: 3, display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <img src={require("../assets/uwuHiBig.png")} />
+          </div>
+
+        </div>
+
       </div >
   );
 }
